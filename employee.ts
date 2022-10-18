09022d03ad8c691abca08cf2e79832c4c564b52c
Grant Ponting <gponting@qualitylogic.com>
2022-10-18 14:53:58 -0700

    [FT-538] Add needed getter function and re-organize functions to be in alphabetical order

--- employee.ts
+++ employee.ts
@@ -10,36 +10,41 @@ type EmployeeData = {
 }
 
 export class Employees {
-  constructor(private readonly prismaEmployee: PrismaClient['employees'])  {}
-
-  async findByEmployeeNo(employeeNo: number): Promise<employees | null> {
-    return this.prismaEmployee.findUnique({ 
-      where: { 
-        emp_no: employeeNo,
-      }
-    })
-  }
+  constructor(private readonly prismaEmployee: PrismaClient['employees']) { }
 
   async addEmployee(data: EmployeeData): Promise<employees | null> {
     return this.prismaEmployee.create({ data: data })
   }
 
+  async deleteEmployees(employeeNumbers: [number]): Promise<number> {
+    return this.prismaEmployee.deleteMany({
+      where: {
+        emp_no: {
+          in: employeeNumbers
+        }
+      }
+    }).then((result) => result.count);
+  }
+
+  async findByEmployeeNo(employeeNo: number): Promise<employees | null> {
+    return this.prismaEmployee.findUnique({
+      where: {
+        emp_no: employeeNo,
+      }
+    })
+  }
+
+  async getLatestAddedEmployeeNo(): Promise<number> {
+    const latest_employee_number = await this.prismaEmployee.findMany({ orderBy: { emp_no: 'desc' } });
+    return latest_employee_number[0].emp_no;
+  }
+
   async updateEmployee(data: EmployeeData): Promise<employees | null> {
-    return this.prismaEmployee.update({ 
+    return this.prismaEmployee.update({
       data: data,
       where: {
         emp_no: data.emp_no
       }
     })
-  }
-
-  async deleteEmployees(employeeNumbers: [number]): Promise<number> {
-    return this.prismaEmployee.deleteMany({ 
-      where: { 
-        emp_no: { 
-          in: employeeNumbers 
-        } 
-      }
-    }).then((result) => result.count);
   }
 }

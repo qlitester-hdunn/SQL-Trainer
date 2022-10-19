d05bf18c504b355e9963e185c76de1565bfb843e
Grant Ponting <gponting@qualitylogic.com>
2022-10-19 13:08:09 -0700

    [FT-543] Add comments to the departments class

--- departments.ts
+++ departments.ts
@@ -8,10 +8,25 @@ type DepartmentData = {
 export class Departments {
     constructor(private readonly prismaDepartment: PrismaClient['departments']) { }
 
+    /**
+    * Adds a new department with the given data.
+    * 
+    * Inputted Data must be formatted as:
+    *   DepartmentData = {
+    *        dept_no: string,
+    *        dept_name: string
+    *    }
+    */
     async addDepartment(data: DepartmentData): Promise<departments | null> {
         return this.prismaDepartment.create({ data: data })
     }
 
+    /**
+    * Deletes every department with the given array of department
+    *   numbers.
+    * 
+    * Note: the entered department number must be an array of strings.
+    */
     async deleteDepartment(departmentNumbers: [string]): Promise<number> {
         return this.prismaDepartment.deleteMany({
             where: {
@@ -22,6 +37,9 @@ export class Departments {
         }).then((result) => result.count);
     }
 
+    /**
+    * Returns the department with the given department number.
+    */
     async findByDepartmentNo(departmentNo: string): Promise<departments | null> {
         return this.prismaDepartment.findUnique({
             where: {
@@ -30,11 +48,28 @@ export class Departments {
         })
     }
 
+    /**
+    * Returns the department number of the most recently added
+    *   department.
+    */
     async getLatestAddedDepartmentNo(): Promise<string> {
         const latest_department_number = await this.prismaDepartment.findMany({ orderBy: { dept_no: 'desc' } });
         return latest_department_number[0].dept_no;
     }
 
+    /**
+    * Updates the department with the given data.
+    * 
+    * Inputted data should be formatted as: 
+    *    DepartmentData = {
+    *        dept_no: string,
+    *        dept_name: string
+    *    }
+    * 
+    * Note: the department being updated is selected from the
+    *   dept_no value in the given data, so this value cannot
+    *   be updated.
+    */
     async updateDepartment(data: DepartmentData): Promise<departments | null> {
         return this.prismaDepartment.update({
             data: data,

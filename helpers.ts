b78ae8967a95770080330c8170f09327527f8510
Grant Ponting <gponting@qualitylogic.com>
2022-10-20 11:04:05 -0700

    [FT-548] Add expect helper function to assist in validation

--- helpers.ts
+++ helpers.ts
@@ -1,2 +1,22 @@
 export const pluralize = (num: number, word: string, plural = word + "s") =>
   [1, -1].includes(Number(num)) ? word : plural;
+
+/**
+* Asserts whether two values are equivalent to each other or not
+*   and returns the number of errors found (1 or 0).
+*
+* If there is an error found it will log the entered values to the console.
+*/
+export function expect(value: any, expected: any) {
+  let error_count = 0;
+  try {
+    if (value !== expected) {
+      error_count = error_count + 1;
+      throw new Error;
+    }
+  } catch (e) {
+    console.log(`\nThe value: "${value}" does not match the expected value: "${expected}".\n`);
+  }
+
+  return error_count;
+}

aca5541f1bdee35ca5be48e7fb23dfe2fc998627
Brian Matejka <bmatejka@qualitylogic.com>
2022-10-21 13:14:25 -0500

    Create test for Salary class addSalary method:
    - add salary data to be used for `add_salary_tests.ts`
    - create tests for `addSalary` method for the Salaries class

---
+++ data/salary_data.ts
@@ -0,0 +1,39 @@
+import { salaries, PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
+
+/**
+* Base salary data to be used in most tests.
+*
+* Note: The emp_no is passed by the call
+*/
+export async function test_salary(
+    emp_no_passed: number,
+    salary_passed: number,
+    from_date_passed: Date,
+    to_date_passed: Date,
+) {
+    const latest_added_employee = await prisma.employees.findMany({ orderBy: { emp_no: 'desc' } });
+    const latest_added_employee_no = latest_added_employee[0].emp_no;
+    const salary_data = {
+        emp_no: emp_no_passed || latest_added_employee_no,
+        salary: salary_passed || 42,
+        from_date: from_date_passed || new Date(),
+        to_date: to_date_passed || new Date(),
+    };
+    return salary_data;
+}
+
+/**
+* Secondary salary data to be used when a second salary is needed.
+*
+* Note: The emp_no is passed by the call
+*/
+export async function secondary_salary(emp_no: number) {
+    const salary_data = {
+      emp_no: emp_no,
+      salary: 35,
+      from_date: new Date(),
+      to_date: new Date(),
+    };
+    return salary_data;
+}

b18187f27d9ee9bdcdb63a8c58a92ec5c6c8bc17
Grant Ponting <gponting@qualitylogic.com>
2022-10-24 10:24:00 -0700

    [FT-566] Add department manager data file

---
+++ data/department_manager_data.ts
@@ -0,0 +1,25 @@
+import { Employees } from "../employee";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+/**
+* Base department employee data to be used in most tests.
+*/
+export async function test_department_manager(
+    dept_no_passed?: string,
+    emp_no_passed?: number,
+    from_date_passed?: Date,
+    to_date_passed?: Date
+) {
+    const employees = new Employees(prisma.employees);
+    const latest_emp_no = await employees.getLatestAddedEmployeeNo();
+
+    const department_employee_data = {
+        emp_no: emp_no_passed || latest_emp_no + 1,
+        dept_no: dept_no_passed || 't001',
+        from_date: from_date_passed || new Date('2022-08-17T00:00:00.000Z'),
+        to_date: to_date_passed || new Date('2022-10-17T00:00:00.000Z')
+    }
+    return department_employee_data;
+}

531d4bfe40ce3bc65c4d56649e6dd218d7f0a91c
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-10-31 11:16:58 -0600

    Quick Test of Cypress. outlined update test

---
+++ cypress/e2e/update_employee.cy.ts
@@ -0,0 +1,13 @@
+import { employees, employees_gender, prisma, PrismaClient } from "@prisma/client";
+
+//start by making employee data for a new employee
+
+//create updated data variable that will change one or more points of data
+
+//ensure no employee is currently created that matches the data to create
+
+//create the new employee using the employee data object
+
+//after adding the employee to the data base, update the employee with the update data object
+
+//check to see if the employee in the database has the same data as the update data object

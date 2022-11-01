df7f61e20e3f2c69e235a3ff21638c3a2d8e2e1b
Newt Clugston <nclugston@qualitylogic.com>
2022-11-01 14:04:37 -0700

    Adding the delete_employee test, adding the getLastEmployeeAdded command.

--- cypress/e2e/delete_employee.cy.ts
+++ cypress/e2e/delete_employee.cy.ts
@@ -8,32 +8,3 @@ const employee_data = {
     gender: employees_gender.M,
     hire_date: '1995-12-17T00:00:00.000Z'
 };
-
-describe("Employee", () => {
-    it("delete Employee", () => {
-
-        cy.deleteEmployee(employee_data.emp_no);
-        //create employee with data above
-        cy.addEmployee(employee_data);
-
-        //after creation, make sure the employee exists in the database
-        cy.findEmployee(employee_data.emp_no).then((foundEmployee) => {
-            expect(foundEmployee.emp_no).to.equal(employee_data.emp_no);
-            expect(foundEmployee.birth_date).to.equal(employee_data.birth_date);
-            expect(foundEmployee.first_name).to.equal(employee_data.first_name);
-            expect(foundEmployee.last_name).to.equal(employee_data.last_name);
-            expect(foundEmployee.gender).to.equal(employee_data.gender);
-            expect(foundEmployee.hire_date).to.equal(employee_data.hire_date);
-        });
-
-        //delete the employee
-        cy.deleteEmployee(employee_data.emp_no);
-
-        //search for employee, ensuring it comes back as null
-        cy.findEmployee(employee_data.emp_no).then((foundEmployee) => {
-            expect(foundEmployee).to.equal(null);
-        });
-    })
-});
-
-

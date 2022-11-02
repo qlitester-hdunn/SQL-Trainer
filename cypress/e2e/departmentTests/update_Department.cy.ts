a0a2667f27c7099dce32076da7ba2bee90ce6b86
Steven Kennedy <skennedy@qualitylogic.com>
2022-11-02 11:40:16 -0600

    Update update_Department.cy.ts

--- cypress/e2e/departmentTests/update_Department.cy.ts
+++ cypress/e2e/departmentTests/update_Department.cy.ts
@@ -5,3 +5,26 @@ const department_data = {
     dept_no: "t800",
     dept_name: "Sky-Net"
 }
+
+const update_data = {
+    dept_no: "t800",
+    dept_name: "Arnold"
+}
+//Begin Test
+describe("Department", () => {
+    it("Update Department", () => {
+        //Ensure department does not exist before creation
+        cy.deleteDepartment(department_data.dept_no);
+        //Create new department
+        cy.addDepartment(department_data);
+        //update new department
+        cy.updateDepartment(update_data);
+        //verify the department contains the updated value, instead of it's original value
+        cy.findDepartment(department_data.dept_no).then((foundDepartment) => {
+            expect(foundDepartment.dept_name).to.equal(update_data.dept_name);
+        });
+        //clean up
+        cy.deleteDepartment(department_data.dept_no);
+
+    })
+});

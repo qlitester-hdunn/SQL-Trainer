550feccea49a14f9f0366a0e0ad6c3335ad5293b
Steven Kennedy <skennedy@qualitylogic.com>
2022-11-03 11:25:02 -0600

    Fixed Title update, it requires same title name

--- cypress/e2e/titleTests/update_Title.cy.ts
+++ cypress/e2e/titleTests/update_Title.cy.ts
@@ -1,7 +1,7 @@
 import { titles, employees, employees_gender, PrismaClient } from "@prisma/client";
 
 const titleData = {
-    emp_no: 50001,
+    emp_no: 500001,
     title: "Overseer",
     from_date: '1995-12-17T00:00:00.000Z',
     to_date: '1999-12-17T00:00:00.000Z'
@@ -15,12 +15,13 @@ const employeeData = {
     gender: employees_gender.M,
     hire_date: '1995-12-17T00:00:00.000Z'
 };
-
+/*apparently you CANNOT update title name, only to_date.
+ title name may act as guid, preventing an update when attempting to change it, even with the existing empNo*/
 const updateData = {
-    emp_no: 50001,
-    title: "Tunnel Snake",
+    emp_no: 500001,
+    title: "Overseer",
     from_date: '1995-12-17T00:00:00.000Z',
-    to_date: '2008-12-17T00:00:00.000Z'
+    to_date: '1997-12-17T00:00:00.000Z'
 }
 
 describe("Title", () => {

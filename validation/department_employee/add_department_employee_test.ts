632203b8d792dac140039e2e227d47473cfded69
Matt Roberts <qlimroberts@github.com>
2022-11-21 14:49:12 -0500

    [FT-650] lint files and improve syntax a bit

--- validation/department_employee/add_department_employee_test.ts
+++ validation/department_employee/add_department_employee_test.ts
@@ -1,91 +1,102 @@
-import { PrismaClient } from '@prisma/client';
-import { Employees } from '../../classes/employee';
-import { Departments } from '../../classes/departments';
-import { Department_Employees } from '../../classes/department_employees';
+import { PrismaClient } from "@prisma/client";
+import { Employees } from "../../classes/employee";
+import { Departments } from "../../classes/departments";
+import { Department_Employees } from "../../classes/department_employees";
 import { test_department } from "../../data/department_data";
-import { test_department_employee } from '../../data/department_employee_data';
-import { test_employee } from '../../data/employee_data';
-import { expect } from '../../helpers';
+import { test_department_employee } from "../../data/department_employee_data";
+import { test_employee } from "../../data/employee_data";
+import { expect } from "../../helpers";
 
 const prisma = new PrismaClient();
 
 async function main(prisma: PrismaClient) {
-    const employees = new Employees(prisma.employees);
-    const departments = new Departments(prisma.departments);
-    const deptartment_employees = new Department_Employees(prisma.dept_emp);
-    const expected_employee_information = await test_employee();
-    const expected_department_information = test_department();
-    const expected_department_employee_information = await test_department_employee(expected_department_information.dept_no);
-    let error_count = 0;
+  const employees = new Employees(prisma.employees);
+  const departments = new Departments(prisma.departments);
+  const deptartment_employees = new Department_Employees(prisma.dept_emp);
+  const expected_employee_information = await test_employee();
+  const expected_department_information = test_department();
+  const expected_department_employee_information =
+    await test_department_employee(expected_department_information.dept_no);
+  let error_count = 0;
 
-    // Clean up step to ensure that the department to be added does not 
-    //  already exist in the database.
-    await departments.deleteDepartment([expected_department_employee_information.dept_no]);
+  // Clean up step to ensure that the department to be added does not
+  //  already exist in the database.
+  await departments.deleteDepartment([
+    expected_department_employee_information.dept_no,
+  ]);
 
-    // Adds necessary data to DB for adding a department employee
-    await departments.addDepartment({
-        dept_no: expected_department_employee_information.dept_no,
-        dept_name: expected_department_information.dept_name
+  // Adds necessary data to DB for adding a department employee
+  await departments.addDepartment({
+    dept_no: expected_department_employee_information.dept_no,
+    dept_name: expected_department_information.dept_name,
+  });
+  await employees.addEmployee({
+    emp_no: expected_department_employee_information.emp_no,
+    birth_date: expected_employee_information.birth_date,
+    first_name: expected_employee_information.first_name,
+    last_name: expected_employee_information.last_name,
+    gender: expected_employee_information.gender,
+    hire_date: expected_employee_information.hire_date,
+  });
+
+  // Adds a department employee
+  await deptartment_employees.addDepartmentEmployee(
+    expected_department_employee_information
+  );
+
+  const department_employee =
+    await deptartment_employees.findByEmployeeNoAndDepartmentNo({
+      emp_no: expected_department_employee_information.emp_no,
+      dept_no: expected_department_employee_information.dept_no,
+      from_date: new Date(),
+      to_date: new Date(),
     });
-    await employees.addEmployee({
-        emp_no: expected_department_employee_information.emp_no,
-        birth_date: expected_employee_information.birth_date,
-        first_name: expected_employee_information.first_name,
-        last_name: expected_employee_information.last_name,
-        gender: expected_employee_information.gender,
-        hire_date: expected_employee_information.hire_date
-    });
 
-    // Adds a department employee
-    await deptartment_employees.addDepartmentEmployee(
-        expected_department_employee_information
-    );
+  // Employee number validation
+  error_count += expect(
+    department_employee?.emp_no,
+    expected_department_employee_information.emp_no
+  );
 
-    const department_employee = await deptartment_employees.findByEmployeeNoAndDepartmentNo(
-        expected_department_employee_information.emp_no,
-        expected_department_employee_information.dept_no
-    );
+  // Department number validation
+  error_count += expect(
+    department_employee?.dept_no,
+    expected_department_employee_information.dept_no
+  );
 
-    // Employee number validation
-    error_count = error_count + expect(
-        department_employee?.emp_no,
-        expected_department_employee_information.emp_no
-    );
+  // From date validation
+  error_count += expect(
+    department_employee?.from_date.toDateString(),
+    expected_department_employee_information.from_date.toDateString()
+  );
 
-    // Department number validation
-    error_count = error_count + expect(
-        department_employee?.dept_no,
-        expected_department_employee_information.dept_no
-    );
+  // To date validation
+  error_count += expect(
+    department_employee?.to_date.toDateString(),
+    expected_department_employee_information.to_date.toDateString()
+  );
 
-    // From date validation
-    error_count = error_count + expect(
-        department_employee?.from_date.toDateString(),
-        expected_department_employee_information.from_date.toDateString()
-    );
+  // Clean Up Step
+  await deptartment_employees.deleteDepartmentEmployee(
+    [expected_department_employee_information.emp_no],
+    [expected_department_employee_information.dept_no]
+  );
+  await employees.deleteEmployees([
+    expected_department_employee_information.emp_no,
+  ]);
+  await departments.deleteDepartment([
+    expected_department_employee_information.dept_no,
+  ]);
 
-    // To date validation
-    error_count = error_count + expect(
-        department_employee?.to_date.toDateString(),
-        expected_department_employee_information.to_date.toDateString()
-    );
-
-    // Clean Up Step
-    await deptartment_employees.deleteDepartmentEmployee(
-        [expected_department_employee_information.emp_no],
-        [expected_department_employee_information.dept_no]
-    );
-    await employees.deleteEmployees([expected_department_employee_information.emp_no])
-    await departments.deleteDepartment([expected_department_employee_information.dept_no]);
-
-    // Logs the number of errors found to the console
-    console.log(`\nNumber of Errors found: ${error_count}\n`);
+  // Logs the number of errors found to the console
+  console.log(`\nNumber of Errors found: ${error_count}\n`);
 }
 
 main(prisma)
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    }).finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- validation/titles/add_title_test.ts
+++ validation/titles/add_title_test.ts
@@ -1,66 +1,78 @@
-import { employees, titles, PrismaClient } from '@prisma/client';
-import { Employees } from '../../classes/employee';
-import { Titles } from '../../classes/title';
-import { test_employee } from '../../data/employee_data';
-import { test_title } from '../../data/title_data';
-import { expect } from '../../helpers';
+import { employees, titles, PrismaClient } from "@prisma/client";
+import { Employees } from "../../classes/employee";
+import { Titles } from "../../classes/title";
+import { test_employee } from "../../data/employee_data";
+import { test_title } from "../../data/title_data";
+import { expect } from "../../helpers";
 
 const prisma = new PrismaClient();
 
 async function main(prisma: PrismaClient) {
-    const employees = new Employees(prisma.employees);
-    const titles = new Titles(prisma.titles);
-    const employee_data = await test_employee();
-    const new_emp_no = employee_data.emp_no;
-    const new_title_date = new Date('1995-12-17T00:00:00.000Z');
-    const expected_title_information = await test_title(new_emp_no, 'Engineer', new_title_date, new_title_date);
-    let error_count = 0;
+  const employees = new Employees(prisma.employees);
+  const titles = new Titles(prisma.titles);
+  const employee_data = await test_employee();
+  const new_emp_no = employee_data.emp_no;
+  const new_title_date = new Date("1995-12-17T00:00:00.000Z");
+  const expected_title_information = await test_title(
+    new_emp_no,
+    "Engineer",
+    new_title_date,
+    new_title_date
+  );
+  let error_count = 0;
 
-    // Clean up step to ensure that the employee & title to be added do not 
-    //  already exist in the database.
-    await employees.deleteEmployees([499991]);
-    await titles.deleteTitles([499991]);
-    await employees.deleteEmployees([new_emp_no]);
-    await titles.deleteTitles([new_emp_no]);
+  // Clean up step to ensure that the employee & title to be added do not
+  //  already exist in the database.
+  await employees.deleteEmployees([499991]);
+  await titles.deleteTitles([499991]);
+  await employees.deleteEmployees([new_emp_no]);
+  await titles.deleteTitles([new_emp_no]);
 
-    // add employee for new title
-    const addedEmployee: employees | null = await employees.addEmployee({
-        emp_no: new_emp_no,
-        birth_date: employee_data.birth_date,
-        first_name: employee_data.first_name,
-        last_name: employee_data.last_name,
-        gender: employee_data.gender,
-        hire_date: employee_data.hire_date,
-    })
+  // add employee for new title
+  const addedEmployee: employees | null = await employees.addEmployee({
+    emp_no: new_emp_no,
+    birth_date: employee_data.birth_date,
+    first_name: employee_data.first_name,
+    last_name: employee_data.last_name,
+    gender: employee_data.gender,
+    hire_date: employee_data.hire_date,
+  });
 
-    // add new title for said employee
-    const new_title_count = await titles.addTitle(expected_title_information);
-    const title: titles | null = await titles.findTitle(new_emp_no);
+  // add new title for said employee
+  const new_title_count = await titles.addTitle(expected_title_information);
+  const title: titles | null = await titles.findTitle(new_emp_no);
 
-     // Employee number validation
-     error_count = error_count + expect(title?.emp_no, expected_title_information.emp_no);
+  // Employee number validation
+  error_count += expect(title?.emp_no, expected_title_information.emp_no);
 
-     // Title validation
-     error_count = error_count + expect(title?.title, expected_title_information.title);
+  // Title validation
+  error_count += expect(title?.title, expected_title_information.title);
 
-     // Title from date validation
-     error_count = error_count + expect(title?.from_date.toDateString(), expected_title_information.from_date.toDateString());
+  // Title from date validation
+  error_count += expect(
+    title?.from_date.toDateString(),
+    expected_title_information.from_date.toDateString()
+  );
 
-     // Title to date validation
-     error_count = error_count + expect(title?.to_date?.toDateString(), expected_title_information.to_date.toDateString());
- 
-     // Clean Up Step
-     await employees.deleteEmployees([new_emp_no]);
-     await titles.deleteTitles([new_emp_no]);
- 
-     // Output results
-     console.log(`\nNumber of Errors found: ${error_count}\n`);
+  // Title to date validation
+  error_count += expect(
+    title?.to_date?.toDateString(),
+    expected_title_information.to_date.toDateString()
+  );
+
+  // Clean Up Step
+  await employees.deleteEmployees([new_emp_no]);
+  await titles.deleteTitles([new_emp_no]);
+
+  // Output results
+  console.log(`\nNumber of Errors found: ${error_count}\n`);
 }
 
 main(prisma)
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    }).finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
